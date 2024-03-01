d921538e83117eee63cbb5ea2c9abb4d48cd6d90
tlinhuwu <161788924+tlinhuwu@users.noreply.github.com>
2024-03-01 10:21:11 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("2jtReRaUKfWWgaA4Gggq47YRANqoieTEACGwcRf5cg7w");
 
 #[program]
 pub mod nft_lend_borrow {
